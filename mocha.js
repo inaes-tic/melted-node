b8d4777a9eeb1ae1622493bd0dba87eeb36b116d
Juan Martin Runge <jmrunge@gmail.com>
2012-12-03 15:50:26 -0300

    Fixed indentation on tests and added test for xml parsing
    Signed-off-by: Juan Martin Runge <jmrunge@gmail.com>

--- mocha.js
+++ mocha.js
@@ -4,60 +4,60 @@ var melted_node = require('./melted-node');
 var mlt = new melted_node();
 
 describe('connects', function(){
-	before(function(done) {
-		var result = mlt.connect();
-		result.then(function() {
-			done();
-		});	
-	})
-	describe('#connected', function(){
-		it('--should return true', function(){
-			assert.equal (mlt.connected, true);
-		})
-	})
-	describe('#no pending messages', function(){
-		it('--should return 0', function(){
-			assert.equal (mlt.pending.length, 0);
-		})
-	})
-	describe('#no errors', function(){
-		it('--should return 0', function(){
-			assert.equal (mlt.errors.length, 0);
-		})
-	})
+    before(function(done) {
+        var result = mlt.connect();
+        result.then(function() {
+            done();
+        });	
+    })
+    describe('#connected', function(){
+        it('--should return true', function(){
+            assert.equal (mlt.connected, true);
+        })
+    })
+    describe('#no pending messages', function(){
+        it('--should return 0', function(){
+            assert.equal (mlt.pending.length, 0);
+        })
+    })
+    describe('#no errors', function(){
+        it('--should return 0', function(){
+            assert.equal (mlt.errors.length, 0);
+        })
+    })
 
 })
 
 describe('commands', function(){
-	describe('#bad and good commands', function(){
-		before(function(done) {
+    describe('#bad and good commands', function(){
+        before(function(done) {
             mlt.sendCommand("no_such_command in my town", "200 OK");
             mlt.sendCommand("load u0 /home/jmrunge/Downloads/Gardel/Mancha-De-Rolando-Antes.mp4", "200 OK");
             mlt.sendCommand("play u0", "200 OK");
             mlt.sendCommand("apnd u0 /home/jmrunge/melted-node/logo.jpg", "200 OK");
-			setTimeout(function() {
-	            done();
-			}, 1000);
-		})
-		it('--should return 1 because of first command', function(){
+            setTimeout(function() {
+                done();
+            }, 1000);
+        })
+        it('--should return 1 because of first command', function(){
             assert.equal (mlt.errors.length, 1);
-		})
-	})
+        })
+    })
 })
 
 describe('queue', function() {
-	describe('#add commands after queue processed', function(){
-		before(function(done) {
+    describe('#add commands after queue processed', function(){
+        before(function(done) {
             mlt.sendCommand("pause u0", "200 OK");
             mlt.sendCommand("play u0", "200 OK");
-			setTimeout(function() {
-	            done();
-			}, 1000);
-		})
-		it('--should return 1 because of previous test', function(){
+            setTimeout(function() {
+                done();
+            }, 1000);
+        })
+        it('--should return 1 because of previous test', function(){
             assert.equal (mlt.errors.length, 1);
-		})
-	})
+        })
+    })
 })
 
 describe('callbacks', function() {
@@ -82,13 +82,13 @@ describe('callbacks', function() {
         })
         it('--incremented error count', function(){
             assert.equal (mlt.errors.length, 2);
-		})
+        })
         it('--received error', function(){
             assert.equal (errorReceived, true);
-		})
+        })
         it('--received response', function(){
             assert.equal (responseReceived, true);
-		})
+        })
     })
 })
 describe('promised command', function() {
@@ -123,18 +123,36 @@ describe('promised command', function() {
         })
         it('--incremented error count', function(){
             assert.equal (mlt.errors.length, 3);
-		})
+        })
         it('--received error for bad command', function(){
             assert.equal (error1Received, true);
-		})
+        })
         it('--received response for bad command', function(){
             assert.equal (response1Received, false);
-		})
+        })
         it('--received error for good command', function(){
             assert.equal (error2Received, false);
-		})
+        })
         it('--received response for good command', function(){
             assert.equal (response2Received, true);
-		})
+        })
+    })
+    
+})
+
+describe('xml', function() {
+    describe('#add xml file with filter', function(){
+        before(function(done) {
+            mlt.sendCommand("load u0 /home/jmrunge/melted-node/melted-test.xml", "200 OK");
+            mlt.sendCommand("play u0", "200 OK");
+            setTimeout(function() {
+                done();
+            }, 1000);
+        })
+        it('--should return 1 because of previous test', function(){
+            assert.equal (mlt.errors.length, 1);
+        })
     })
 })
+
+
