bd1f15dddb9415f1579fec74e59143f4aaf7c65d
Juan Martin Runge <jmrunge@gmail.com>
2013-04-10 13:23:18 -0300

    Added initial stress test
    Signed-off-by: Juan Martin Runge <jmrunge@gmail.com>

--- mocha.js
+++ mocha.js
@@ -3,6 +3,24 @@ var assert = require("assert");
 var melted_node = require('./melted-node');
 var mlt = new melted_node('localhost', 5250);
 
+// SILENCE LOG OUTPUT
+/*var util = require('util');
+var fs = require('fs');
+var log = fs.createWriteStream('/home/jmrunge/stdout.log');
+
+console.log = console.info = function(t) {
+  var out;
+  if (t && ~t.indexOf('%')) {
+    out = util.format.apply(util, arguments);
+    process.stdout.write(out + '\n');
+    return;
+  } else {
+    out = Array.prototype.join.call(arguments, ' ');
+  }
+  out && log.write(out + '\n');
+};*/
+// END SILENCE LOG OUTPUT
+ 
 describe('connects', function(){
     before(function(done) {
         var result = mlt.connect();
@@ -11,24 +29,24 @@ describe('connects', function(){
         }, function(err) {
             console.error("Error: " + err);
         });	
-    })
+    });
     describe('#connected', function(){
         it('--should return true', function(){
             assert.equal (mlt.connected, true);
-        })
-    })
+        });
+    });
     describe('#no pending messages', function(){
         it('--should return 0', function(){
             assert.equal (mlt.pending.length, 0);
-        })
-    })
+        });
+    });
     describe('#no errors', function(){
         it('--should return 0', function(){
             assert.equal (mlt.errors.length, 0);
-        })
-    })
+        });
+    });
 
-})
+});
 
 describe('commands', function(){
     describe('#bad and good commands', function(){
@@ -40,12 +58,12 @@ describe('commands', function(){
             setTimeout(function() {
                 done();
             }, 1000);
-        })
+        });
         it('--should return 1 because of first command', function(){
             assert.equal (mlt.errors.length, 1);
-        })
-    })
-})
+        });
+    });
+});
 
 describe('queue', function() {
     describe('#add commands after queue processed', function(){
@@ -55,12 +73,12 @@ describe('queue', function() {
             setTimeout(function() {
                 done();
             }, 1000);
-        })
+        });
         it('--should return 1 because of previous test', function(){
             assert.equal (mlt.errors.length, 1);
-        })
-    })
-})
+        });
+    });
+});
 
 describe('callbacks', function() {
     describe('#execute callback function on success and on error', function() {
@@ -73,7 +91,7 @@ describe('callbacks', function() {
                 errorReceived = true;
                 done();
             };
-        })
+        });
         before(function(done) {
             mlt.sendCommand("list u0", "201 OK", callback);
             function callback(response) {
@@ -81,18 +99,18 @@ describe('callbacks', function() {
                 responseReceived = true;
                 done();
             };
-        })
+        });
         it('--incremented error count', function(){
             assert.equal (mlt.errors.length, 2);
-        })
+        });
         it('--received error', function(){
             assert.equal (errorReceived, true);
-        })
+        });
         it('--received response', function(){
             assert.equal (responseReceived, true);
-        })
-    })
-})
+        });
+    });
+});
 describe('promised command', function() {
     describe('#receive promised object', function() {
         var error1Received = false;
@@ -110,7 +128,7 @@ describe('promised command', function() {
                 error1Received = true;
                 done();
             });
-        })
+        });
         before(function(done) {
             var result = mlt.sendPromisedCommand("uls", "201 OK");
             result.then(function(response) {
@@ -122,25 +140,24 @@ describe('promised command', function() {
                 error2Received = true;
                 done();
             });
-        })
+        });
         it('--incremented error count', function(){
             assert.equal (mlt.errors.length, 3);
-        })
+        });
         it('--received error for bad command', function(){
             assert.equal (error1Received, true);
-        })
+        });
         it('--received response for bad command', function(){
             assert.equal (response1Received, false);
-        })
+        });
         it('--received error for good command', function(){
             assert.equal (error2Received, false);
-        })
+        });
         it('--received response for good command', function(){
             assert.equal (response2Received, true);
-        })
-    })
-    
-})
+        });
+    });    
+});
 
 describe('xml', function() {
     describe('#add xml file with filter', function(){
@@ -150,17 +167,40 @@ describe('xml', function() {
             setTimeout(function() {
                 done();
             }, 1000);
-        })
+        });
         it('--should return 3 because of previous test', function(){
             assert.equal(mlt.errors.length, 3);
-        })
+        });
+    });
+});
+
+describe('stress', function() {
+    describe('#obtain status 100 times', function() {
+        before(function(done) {
+            var count = 0;
+            setInterval(function() {
+                if (count === 100) {
+                    clearInterval(this);
+                    done();
+                }
+                mlt.sendCommand("usta u0", "202 OK", function(response) {
+                    console.log(response);
+                    console.log("PASADA NRO: " + count);
+                }, function(error) {
+                    console.error(error);
+                });
+                count++;
+            }, 500);
+        });
+        it('--should return 3 (no more errors!)', function(){
+            assert.equal(mlt.errors.length, 3);
+        });
         after(function(done) {
-            setTimeout(function() {
-                mlt.sendCommand("stop u0");
-                done();
-            }, 2500);
-        })
-    })
-})
-
-
+//            setTimeout(function() {
+                mlt.sendCommand("stop u0", "200 OK", function(result) {
+                    done();
+                });
+//            }, 2500);
+        });
+    }) ;
+});
